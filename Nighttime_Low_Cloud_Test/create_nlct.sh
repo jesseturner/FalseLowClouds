6e1be59cc57ac8ce94b0ff7ede4f2830b227f512
Jesse Turner <jesseturner72@gmail.com>
2023-08-18 20:46:04 +0000

    NLCT data is now created separately

--- Nighttime_Low_Cloud_Test/create_nlct.sh
+++ Nighttime_Low_Cloud_Test/create_nlct.sh
@@ -1,13 +1,17 @@
-#None of this is correct yet
-for date in '20220914'
+#! /usr/bin/bash
 
-do 
+abi_path="/home/jturner/false_low_clouds_2023/Static_Features/20220914"
+#b07_filename="OR_ABI-L1b-RadF-M6C07_G16_s20222570240204_e20222570249524_c20222570249569.nc"
+nlct_path="/home/jturner/false_low_clouds_2023/Nighttime_Low_Cloud_Test/data/"
 
-	if [ -d time_$dtime/$date ]; then
-	echo "directory exists"
+echo "Enter your b07 filename (Static_Features/20220914): "
+read b07_filename
 
-	python data_download_convert.py $date $dtime
+if [ -e $abi_path/$b07_filename ]; then
+echo "Success --- data found, proceeding"
 
+python create_nlct_func.py $abi_path $b07_filename $nlct_path
 
-	fi
-done
+else echo "Failure --- data not found"
+
+fi
