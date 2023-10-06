60fb2cc952cfa049319421417dd2bc580c39117d
Jesse Turner <jesseturner72@gmail.com>
2023-10-06 16:13:21 +0000

    Creating anomaly analysis for OISST and Buoy

--- Case_Studies/Georges_Bank/data_download_sst.sh
+++ Case_Studies/Georges_Bank/data_download_sst.sh
@@ -1,21 +1,24 @@
 #! /usr/bin/bash
+for month in 01 02 03 04 05 06 07 08 09 10 11 12
+do
 
-for date in 20230101 20230102 20230103 20230104 20230105 20230106 20230107 20230108 20230109 20230110 20230111 20230112 20230113 20230114 20230115 20230116 20230117 20230118 20230119 20230120 20230121 20230122 20230123 20230124 20230125 20230126 20230127 20230128 20230129 20230130 20230131
+    for date in 2020"$month"01 2020"$month"02 2020"$month"03 2020"$month"04 2020"$month"05 2020"$month"06 2020"$month"07 2020"$month"08 2020"$month"09 2020"$month"10 2020"$month"11 2020"$month"12 2020"$month"13 2020"$month"14 2020"$month"15 2020"$month"16 2020"$month"17 2020"$month"18 2020"$month"19 2020"$month"20 2020"$month"21 2020"$month"22 2020"$month"23 2020"$month"24 2020"$month"25 2020"$month"26 2020"$month"27 2020"$month"28 2020"$month"29 2020"$month"30 2020"$month"31
 
-do 
+    do 
 
-    sst_name=oisst-avhrr-v02r01."$date".nc
+        sst_name=oisst-avhrr-v02r01."$date".nc
 
-	sst_source=https://www.ncei.noaa.gov/thredds/fileServer/OisstBase/NetCDF/V2.1/AVHRR/${date:0:6}/oisst-avhrr-v02r01."$date".nc
-    sst_source_backup=https://www.ncei.noaa.gov/thredds/fileServer/OisstBase/NetCDF/V2.1/AVHRR/${date:0:6}/oisst-avhrr-v02r01."$date"_preliminary.nc
-    
+            sst_source=https://www.ncei.noaa.gov/thredds/fileServer/OisstBase/NetCDF/V2.1/AVHRR/${date:0:6}/oisst-avhrr-v02r01."$date".nc
+        sst_source_backup=https://www.ncei.noaa.gov/thredds/fileServer/OisstBase/NetCDF/V2.1/AVHRR/${date:0:6}/oisst-avhrr-v02r01."$date"_preliminary.nc
 
-	if [ -f SST_data/$sst_name ]; then
-	echo "file exists"
-	
-    else
-    wget $sst_source -P SST_data || wget $sst_source_backup -P SST_data
 
-	fi
+        if [ -f OISST_data/$sst_name ]; then
+        echo "file exists"
 
+        else
+        wget $sst_source -P OISST_data || wget $sst_source_backup -P OISST_data
+
+        fi
+
+    done
 done
