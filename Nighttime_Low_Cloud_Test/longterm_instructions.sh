2bfa365ca33e4fab501e1dc1be3b9cac0f83065b
Jesse Turner <jesseturner72@gmail.com>
2023-11-03 20:23:15 +0000

    Working on static elements method

--- Nighttime_Low_Cloud_Test/longterm_instructions.sh
+++ Nighttime_Low_Cloud_Test/longterm_instructions.sh
@@ -7,7 +7,7 @@ ssh-keygen -t rsa -b 2048
 ssh-copy-id $remote_user@$remote_host
 chmod 600 ~/.ssh/id_rsa
 
-for date in 20230907; #20230908 20230909; do 
+for date in 20230910 20230911 20230912 20230913 20230914; do 
 
     julian=$(date -d "$date" +%j)
 
@@ -16,12 +16,13 @@ for date in 20230907; #20230908 20230909; do
     
     local_destination="abi_data_temp/"$date
 
-    if [ ! -d local_destination ]; then mkdir local_destination
+    if [ ! -d $local_destination ]; then mkdir $local_destination
     fi
     
-    for time in "00" "01" "02" "03"; do
-         remote_directory_07="/mnt/grb/goes16/"${date:0:4}/${date:0:4}_${date:4:2}_${date:6:2}_$julian"/abi/L1b/RadF/OR_ABI-L1b-RadF-M6C07_G16_s"${date:0:4}$julian$time*
-         remote_directory_14="/mnt/grb/goes16/"${date:0:4}/${date:0:4}_${date:4:2}_${date:6:2}_$julian"/abi/L1b/RadF/OR_ABI-L1b-RadF-M6C14_G16_s"${date:0:4}$julian$time*
+    #--- Added zeros at the end for 'top of each hour'
+    for time in "00" "01" "02" "03" "04" "05" "06" "07" "08"; do
+         remote_directory_07="/mnt/grb/goes16/"${date:0:4}/${date:0:4}_${date:4:2}_${date:6:2}_$julian"/abi/L1b/RadF/OR_ABI-L1b-RadF-M6C07_G16_s"${date:0:4}$julian$time"0"*
+         remote_directory_14="/mnt/grb/goes16/"${date:0:4}/${date:0:4}_${date:4:2}_${date:6:2}_$julian"/abi/L1b/RadF/OR_ABI-L1b-RadF-M6C14_G16_s"${date:0:4}$julian$time"0"*
     
         scp $remote_user@$remote_host:$remote_directory_07 $local_destination
         scp $remote_user@$remote_host:$remote_directory_14 $local_destination
