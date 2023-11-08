bd2d353188e5d895a4a257c4caadabd60bc2e192
Jesse Turner <jesseturner72@gmail.com>
2023-11-08 20:32:58 +0000

    Now able to run 2 night static features

--- Nighttime_Low_Cloud_Test/longterm_instructions.sh
+++ Nighttime_Low_Cloud_Test/longterm_instructions.sh
@@ -7,7 +7,7 @@ ssh-keygen -t rsa -b 2048
 ssh-copy-id $remote_user@$remote_host
 chmod 600 ~/.ssh/id_rsa
 
-for date in 20230909; do 
+for date in 20230919 20230920 20230921 20230922 20230923 20230924; do 
 
     julian=$(date -d "$date" +%j)
 
@@ -22,18 +22,17 @@ for date in 20230909; do
     #--- Added zeros at the end for 'top of each hour'
     #------ This seems be getting stuck in a loop, it runs the same time multiple times
     
-    #for time in "00" "01" "02" "03" "04" "05" "06" "07" "08"; do
-    for time in "04" "05" "06" "07" "08"; do
+    for time in "00" "01" "02" "03" "04" "05" "06" "07" "08"; do
          remote_directory_07="/mnt/grb/goes16/"${date:0:4}/${date:0:4}_${date:4:2}_${date:6:2}_$julian"/abi/L1b/RadF/OR_ABI-L1b-RadF-M6C07_G16_s"${date:0:4}$julian$time"0"*
          remote_directory_14="/mnt/grb/goes16/"${date:0:4}/${date:0:4}_${date:4:2}_${date:6:2}_$julian"/abi/L1b/RadF/OR_ABI-L1b-RadF-M6C14_G16_s"${date:0:4}$julian$time"0"*
     
         scp $remote_user@$remote_host:$remote_directory_07 $local_destination
         scp $remote_user@$remote_host:$remote_directory_14 $local_destination
-    
-        chmod +x longterm_create_nlct.sh
-        ./longterm_create_nlct.sh "$date"
         
     done
+    
+    chmod +x longterm_create_nlct.sh
+    ./longterm_create_nlct.sh "$date"
     
     rm -r $local_destination
     
