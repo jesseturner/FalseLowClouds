4057f8e3bdcbdd5fd65f91975a68c13f644ae5cc
Jesse Turner <jesseturner72@gmail.com>
2024-01-18 21:55:38 +0000

    FLCI seasonal climatology running

--- Nighttime_Low_Cloud_Test/collect_nlct.sh
+++ Nighttime_Low_Cloud_Test/collect_nlct.sh
@@ -32,6 +32,8 @@ while [ "$date" -le "$(date -d "$end_date" +%Y%m%d)" ]; do
     for time in $hours; do
          remote_directory_07="/mnt/grb/goes16/"${date:0:4}/${date:0:4}_${date:4:2}_${date:6:2}_$julian"/abi/L1b/RadF/OR_ABI-L1b-RadF-M6C07_G16_s"${date:0:4}$julian$time"0"*
          remote_directory_14="/mnt/grb/goes16/"${date:0:4}/${date:0:4}_${date:4:2}_${date:6:2}_$julian"/abi/L1b/RadF/OR_ABI-L1b-RadF-M6C14_G16_s"${date:0:4}$julian$time"0"*
+         
+#--- M6C13 has the clean IR window, 10.3um
 
         scp $remote_user@$remote_host:$remote_directory_07 $local_destination
         scp $remote_user@$remote_host:$remote_directory_14 $local_destination
